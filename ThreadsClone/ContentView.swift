e52fa8a8aa2412ec6ec09e404e24b3fb5cc1ec32
ichichavez <isidrochavez429@gmail.com>
2024-05-13 22:32:31 -0400

    Initial Commit

---
+++ ThreadsClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ThreadsClone
+//
+//  Created by Isidro Chávez on 2024-05-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Isidro Chávez on 2024-05-13.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
